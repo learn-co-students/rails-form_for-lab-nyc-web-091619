d252d0dd9d038a5071c722df964ea17d92352ed8
VPoshelyuk <aslava198@gmail.com>
2019-10-13 12:04:23 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :school_classes, :students, except: [:destroy, :index]
+  # get '/school_classes/:id', to: 'school_classes#show', as: 'school_class_path'
+
+  patch 'school_classes/:id', to: 'school_classes#update'
+  patch 'students/:id', to: 'students#update'
 end
