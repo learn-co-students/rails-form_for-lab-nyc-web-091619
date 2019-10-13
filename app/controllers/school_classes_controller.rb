1a81fc5b1267e4ec29a539d571632bfeb15e4c0c
Liam <liam.healy1@marist.edu>
2019-10-13 18:53:05 -0400

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,40 @@
+require 'pry'
+class SchoolClassesController < ApplicationController
+
+    def index
+        @school_classes = SchoolClass.all
+    end
+
+    def show
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+    def new
+    end
+
+    def create
+        @school_class = SchoolClass.new
+        @school_class.title = params[:school_class][:title]
+        @school_class.room_number = params[:school_class][:room_number]
+        @school_class.save
+        redirect_to school_class_path(@school_class)
+    end
+ 
+    def edit
+        @school_class = SchoolClass.find(params[:id])
+    end
+    
+    def update
+        # binding.pry
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.update(params.require(:school_class).permit(:title, :room_number))
+        redirect_to school_class_path(@school_class)
+    end
+
+    private
+ 
+    def post_params
+        params.require(:school_class).permit(:title, :room_number)
+    end
+
+end
