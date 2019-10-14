b453b8cbac6418f0c6ee61816d74496012660416
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-10-13 21:35:31 -0400

    met all specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :students, only: %i[index show edit update new create]
+  resources :school_classes, only: %i[index show edit update new create]
+  
 end
